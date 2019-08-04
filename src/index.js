d5e9628e45e553940132072d44d58fcc04853b62
Sven Liebig <liebigsv@gmail.com>
2019-08-04 21:31:39 +0200

    example

--- src/index.js
+++ src/index.js
@@ -5,10 +5,25 @@ const app = express()
 app.use(parameterRewriter({ hello: 'bye' })) // usage
 
 app.use((req, res, next) => {
+	console.log("2 - after parameter rewriter")
     console.log(req.url)
     console.log(req.originalUrl)
     console.log(req.query)
     next()
+})
+
+// app.use(function(req, res, next) {
+// 	const temp = res.jsonp
+// 	res.jsonp = function(body) {
+// 		console.log("intecepting budddyyyyyy: ", body)
+// 		temp.call(this, { ...body, niceru: "add data" })
+// 	}
+// 	next();
+// })
+
+app.use("/", (req, res, next) => {
+	res.jsonp({ "hello": "world "})
+	next()
 })
 
 app.listen(3000, '0.0.0.0', () => {
